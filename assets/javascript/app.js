50840c51c556ff3f54e2de6d7f8c47b0a1fb8b64
Nelly Ackerman <pavel.ack@gmail.com>
2018-03-15 21:00:24 -0400

    changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,15 @@
 var time = 30;
 var questions_index = 0;
-var questions_arr = [["What was the first full length CGI movie?","A Bug's Life","Monsters Inc","Toy Story","The Lion King"],
+var questions_arr = [
+["What was the first full length CGI movie?","A Bug's Life","Monsters Inc","Toy Story","The Lion King"],
 ["Which of these is NOT a name of one of the Spice Girls?","Sporty Spice","Fred Spice","Scary Spice","Posh Spice"],
 ["Which NBA team won the most titles in the 90s?","New York Knicks","Portland Trailblazers","Los Angeles Lakers",
 "Chicago Bulls"],["Which group released the hit song Smells Like Teen Spirit?", "Nirvana","Backstreet Boys","Offspring","No Doubt"],
 ["Which popular Disney movie featured the song 'Circle of life'?","Alladin","Hercules","Mulan","The Lion King"],
 ["Finish this line from the Fresh Prince of Bel-Air theme song 'I whistled for a cab and when it came near, the licence plate said ...'",
 "Dice","Mirror","Fresh","Cab"],["What was Doug's best friend's name?","Skeeter","Mark","Zack","Cody"],
-["What was the name of the principal at Bayside High in Saved By The Bell?","Mr. Zhou","Mr. Driggers","Mr. Belding","Mr. Page"]];
+["What was the name of the principal at Bayside High in Saved By The Bell?","Mr. Zhou","Mr. Driggers","Mr. Belding","Mr. Page"]
+];
 
 
 var answers = ["Toy Story","Fred Spice","Chicago Bulls","Nirvana","The Lion King","Fresh","Skeeter","Mr. Belding"];
@@ -45,11 +47,11 @@ function ask(){
 	},1000);
 	var arr = questions_arr[questions_index];
 	
-	elem = $('<p style="font-size:30px,font-weight:bold;color:#212730">' + arr[0] + '</p>');
+	elem = $('<p style="font-size:30px;font-weight:bold;color:#212730">' + arr[0] + '</p>');
 	$("#questions").append(elem);
 	
 	for(var i = 1; i < arr.length; i++){
-		var html = '<div><input type="radio" name="movie" onclick="answer(this);" value="'+arr[i]+'"><label for="'+arr[i]+'">'+	
+		var html = '<div><input type="radio" name="movie" onclick="answer(this);" value="'+arr[i]+'"><label style="font-size:20px;font-weight:bold;color:red" for="'+arr[i]+'">'+	
        arr[i]+	'</label></div>';
 		$('#questions').append($(html));
 	}
@@ -60,7 +62,7 @@ function answer(obj){
 	var ans = $(obj).val();
 
 	$('#questions').html('');
-	var e = $('<div onclick="next()"></div>')
+	var e = $('<div onclick="next()"></div>');
 	if(ans == answers[questions_index]){
 		e.append($('<p style="font-size:30px;font-weight:bold;color:#f442e8">Correct!!!</p>'));
 	} else {
